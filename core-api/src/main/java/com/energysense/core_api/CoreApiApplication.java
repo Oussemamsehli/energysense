bc02510a05cda180080ad28901087ba74ec01dfe
Oussema Msehli <oussema.msehli@esprit.tn>
2026-06-19 16:22:15 +0100

    fix(mqtt): move @EnableIntegration to main class, restore @ServiceActivator

--- core-api/src/main/java/com/energysense/core_api/CoreApiApplication.java
+++ core-api/src/main/java/com/energysense/core_api/CoreApiApplication.java
@@ -2,12 +2,12 @@ package com.energysense.core_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.integration.annotation.EnableIntegration;
 
 @SpringBootApplication
+@EnableIntegration
 public class CoreApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CoreApiApplication.class, args);
 	}
-
 }
