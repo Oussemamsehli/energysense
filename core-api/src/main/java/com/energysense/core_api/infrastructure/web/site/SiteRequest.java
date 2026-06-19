e985fdae1c790fbeabb1b5b8e93ba0dff5f5286b
Oussema Msehli <oussema.msehli@esprit.tn>
2026-06-19 15:25:29 +0100

    feat(site): add SiteRequest DTO

---
+++ core-api/src/main/java/com/energysense/core_api/infrastructure/web/site/SiteRequest.java
@@ -0,0 +1,12 @@
+package com.energysense.core_api.infrastructure.web.site;
+
+public class SiteRequest {
+
+    private String name;
+    private String location;
+
+    public SiteRequest() {}
+
+    public String getName() { return name; }
+    public String getLocation() { return location; }
+}
